dd5bfd2724b6b877ae6a795671ed34052bf95ddc
Andrii <dron_99@icloud.com>
2024-02-15 18:11:37 +0100

    fixed

--- Task_1_2.py
+++ Task_1_2.py
@@ -27,7 +27,7 @@ print(fib(15))
 
 #Задача друга
 def generator_numbers(text: str):
-    numbers = map(float, filter(lambda x: re.match(r"\d+[\.,]{0,1}\d+.",x),text.split(" ")))
+    numbers = map(float, filter(lambda x: re.match(r"\b\d+(\.\d+)?\b",x),text.split(" ")))
     for number in numbers:
         yield number
 
@@ -35,7 +35,7 @@ def sum_profit(text: str, func: Callable):
     return reduce(lambda x,y:x+y ,func(text))
 
 if __name__=="__main__":
-    text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+    text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний 5.5 дохід, доповнений додатковими 0.02 надходженнями 27.45 і 324.00 доларів."
     total_income = sum_profit(text, generator_numbers)
     print(f"Загальний дохід: {total_income}")
 
